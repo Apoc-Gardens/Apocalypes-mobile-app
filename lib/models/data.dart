b68c31c0ba1887ddbf9d4f7e0a81ceb7369661c6
sahan-panditharathne <72350418+sahan-panditharathne@users.noreply.github.com>
2024-05-16 13:41:34 +0530

    modified data model

--- lib/models/data.dart
+++ lib/models/data.dart
@@ -1,15 +1,29 @@
+// models/data.dart
 class Data {
+  final int id;
   final int deviceId;
   final int dataTypeId;
   final double value;
 
-  Data({required this.deviceId, required this.dataTypeId, required this.value});
+  Data({required this.id, required this.deviceId, required this.dataTypeId, required this.value});
 
+  // Convert a Data into a Map. The keys must correspond to the column names in the database.
   Map<String, dynamic> toMap() {
     return {
+      'id': id,
       'device_id': deviceId,
       'datatype_id': dataTypeId,
       'value': value,
     };
+  }
+
+  // Convert a Map into a Data. The keys must correspond to the column names in the database.
+  factory Data.fromMap(Map<String, dynamic> map) {
+    return Data(
+      id: map['id'],
+      deviceId: map['device_id'],
+      dataTypeId: map['datatype_id'],
+      value: map['value'],
+    );
   }
 }

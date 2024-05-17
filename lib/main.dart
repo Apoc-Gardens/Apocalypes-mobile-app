190874d28aad69c05f11899c378cf7dec5538e21
sahan-panditharathne <72350418+sahan-panditharathne@users.noreply.github.com>
2024-05-17 16:43:55 +0530

    with this modification, when user opens the app, it will check is receiver devices are saved, if so the user will be taken straight to the sensors page

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:provider/provider.dart';
 import 'package:mybluetoothapp/pages/landing_page.dart';
 import 'package:mybluetoothapp/pages/scan_devices.dart';
 import 'package:mybluetoothapp/pages/sensors.dart';
 import 'package:mybluetoothapp/providers/bluetooth_provider.dart';
 import 'package:mybluetoothapp/test/dbpage.dart';
-import 'package:provider/provider.dart';
 import 'package:mybluetoothapp/pages/characteristics.dart';
+import '../services/database_service.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+
+  Future<int> getReceiverCount() async {
+    return await databaseHelper.getReceiverCount() ?? 0;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [ChangeNotifierProvider(create: (BuildContext context) { BluetoothProvider(); },)],
-      child: MaterialApp(
-        initialRoute: '/',
-        routes: {
-          '/': (context) => dbpage(),
-          '/scan': (context) => MyHomePage(),
-          '/sensors': (context) => Sensors(),
-          '/characteristics': (context) => CharacteristicViewer(connectedDevice: ModalRoute.of(context)!.settings.arguments as BluetoothDevice),
-        },
-      ),
+    return FutureBuilder<int>(
+      future: getReceiverCount(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        } else {
+          final receiverCount = snapshot.data ?? 0;
+          return MultiProvider(
+            providers: [
+              ChangeNotifierProvider(create: (_) => BluetoothProvider()),
+            ],
+            child: MaterialApp(
+              initialRoute: receiverCount == 0 ? '/' : '/sensors',
+              routes: {
+                '/': (context) => Welcome(),
+                '/scan': (context) => MyHomePage(),
+                '/sensors': (context) => Sensors(),
+                '/characteristics': (context) => CharacteristicViewer(
+                    connectedDevice: ModalRoute.of(context)!.settings.arguments as BluetoothDevice),
+                '/test': (context) => dbpage(),
+              },
+            ),
+          );
+        }
+      },
     );
   }
 }

d603db1a526cad739082fce4d54e7d9adc56f672
sahan-panditharathne <72350418+sahan-panditharathne@users.noreply.github.com>
2024-05-27 12:13:41 +0530

    on the way to displaying the last update time and the number of data

--- lib/widgets/sensor_card.dart
+++ lib/widgets/sensor_card.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:mybluetoothapp/models/node.dart';
+import 'package:mybluetoothapp/services/database_service.dart';
 
 class SensorCard extends StatefulWidget {
-  const SensorCard({super.key});
+  final Node node;
+  const SensorCard({super.key, required this.node});
 
   @override
   State<SensorCard> createState() => _SensorCardState();
 }
 
 class _SensorCardState extends State<SensorCard> {
+  DatabaseHelper databaseHelper = DatabaseHelper();
+  late int noOfData;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<int?> dataCount(){
+    return databaseHelper.countData(widget.node.id.toString());
+  }
+
+  Future<int?> getLatestTime(){
+    return databaseHelper.latestDataTimeStamp(widget.node.id.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -21,17 +40,17 @@ class _SensorCardState extends State<SensorCard> {
       child: Container(
         color: Colors.white,
         padding: const EdgeInsets.all(16.0), // Padding
-        child: const Column(
+        child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Carrot Patch',
+            Text(widget.node.name,
               style: TextStyle(
                 fontSize: 18,
                 fontWeight: FontWeight.bold,
                 color: Colors.black
               ),
             ),
-            Text('Behind the main house',
+            Text(widget.node.description ?? ' ',
               style: TextStyle(
                   fontSize: 10,
                   fontWeight: FontWeight.normal,
@@ -45,7 +64,7 @@ class _SensorCardState extends State<SensorCard> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text('Last Update'),
-                    Text('6 min ago')
+                    Text(getLatestTime().toString())
                   ],
                 ),
                 SizedBox(width: 20,),
@@ -53,7 +72,7 @@ class _SensorCardState extends State<SensorCard> {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text('No. Data'),
-                    Text('300')
+                    Text(dataCount().toString())
                   ],
                 )
               ],

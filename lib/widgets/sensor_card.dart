332bc879202fa1c1cead566cccf0d458d2dfdd89
sahan-panditharathne <72350418+sahan-panditharathne@users.noreply.github.com>
2024-05-28 13:23:53 +0530

    added functionality to display latest update time and number of data point

--- lib/widgets/sensor_card.dart
+++ lib/widgets/sensor_card.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mybluetoothapp/models/node.dart';
+import 'package:mybluetoothapp/pages/view_sensor.dart';
 import 'package:mybluetoothapp/services/database_service.dart';
+
+import '../models/data.dart';
 
 class SensorCard extends StatefulWidget {
   final Node node;
@@ -12,114 +15,139 @@ class SensorCard extends StatefulWidget {
 
 class _SensorCardState extends State<SensorCard> {
   DatabaseHelper databaseHelper = DatabaseHelper();
-  late int noOfData;
+  int noOfData = 0;
+  int latestTime = 0;
+  late List<Data> latestReadings;
 
   @override
-  void initState() {
+  initState() {
     super.initState();
+    getData();
   }
 
-  Future<int?> dataCount(){
-    return databaseHelper.countData(widget.node.id.toString());
+  Future<void> getData() async {
+    await dataCount();
+    await getLatestTime();
+    await getLatestReadings();
+    setState(() {});
   }
 
-  Future<int?> getLatestTime(){
-    return databaseHelper.latestDataTimeStamp(widget.node.id.toString());
+  Future<void> dataCount() async {
+    noOfData = await databaseHelper.countData(widget.node.id.toString()) ?? 0;
+  }
+
+  Future<void> getLatestTime() async {
+    latestTime =  await databaseHelper.latestDataTimeStamp(widget.node.id.toString()) ?? 0;
+  }
+
+  Future<void> getLatestReadings() async{
+    latestReadings = await databaseHelper.getLatestReadings(widget.node.id.toString());
+    print(latestReadings);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(8.0), // Border radius
-        side: const BorderSide(
-          color: Color(0xFFE4E4E4), // Border color
-          width: 1.0, // Border thickness
+    return GestureDetector(
+      onTap: (){
+        /*Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => ViewSensor(node: widget.node),
+          ),
+        );*/
+      },
+      child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0), // Border radius
+          side: const BorderSide(
+            color: Color(0xFFE4E4E4), // Border color
+            width: 1.0, // Border thickness
+          ),
         ),
-      ),
-      child: Container(
-        color: Colors.white,
-        padding: const EdgeInsets.all(16.0), // Padding
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(widget.node.name,
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Colors.black
-              ),
-            ),
-            Text(widget.node.description ?? ' ',
-              style: TextStyle(
-                  fontSize: 10,
-                  fontWeight: FontWeight.normal,
+        child: Container(
+          color: Colors.white,
+          padding: const EdgeInsets.all(16.0), // Padding
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(widget.node.name,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
                   color: Colors.black
+                ),
               ),
-            ),
-            SizedBox(height: 10,),
-            Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Last Update'),
-                    Text(getLatestTime().toString())
-                  ],
+              Text(widget.node.description ?? ' ',
+                style: TextStyle(
+                    fontSize: 10,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.black
                 ),
-                SizedBox(width: 20,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('No. Data'),
-                    Text(dataCount().toString())
-                  ],
-                )
-              ],
-            ),
-            SizedBox(height: 10,),
-            Divider(),
-            Text('LATEST READINGS',
-            style: TextStyle(
-              color: Color(0xFF0AA061),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Temperarure'),
-                    Text('30C')
-                  ],
+              SizedBox(height: 10,),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Last Update'),
+                      Text(latestTime.toString())
+                    ],
+                  ),
+                  SizedBox(width: 20,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('No. Data'),
+                      Text(noOfData.toString())
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 10,),
+              Divider(),
+              Text('LATEST READINGS',
+              style: TextStyle(
+                color: Color(0xFF0AA061),
                 ),
-                SizedBox(width: 20,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Humidity'),
-                    Text('30%')
-                  ],
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('LUX'),
-                    Text('15467')
-                  ],
-                ),
-                SizedBox(width: 20,),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Soil'),
-                    Text('100%')
-                  ],
-                )
-              ],
-            )
-          ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Temperarure'),
+                      Text('30C')
+                    ],
+                  ),
+                  SizedBox(width: 20,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Humidity'),
+                      Text('30%')
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('LUX'),
+                      Text('15467')
+                    ],
+                  ),
+                  SizedBox(width: 20,),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Soil'),
+                      Text('100%')
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );

cafabee3b46b1f2a77a472284589dc3f92bad5d6
sahan-panditharathne <72350418+sahan-panditharathne@users.noreply.github.com>
2024-06-03 17:01:11 +0530

    added a side menu bar

--- lib/navigation/side_menu.dart
+++ lib/navigation/side_menu.dart
@@ -12,7 +12,34 @@ class _SideMenuState extends State<SideMenu> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-
+        child: ListView(
+          children: [
+            DrawerHeader(
+              child: Text('APOC GARDENS',
+              style: TextStyle(
+                color: Color(0xFF0AA061),
+                fontSize:20,
+              ),
+            ),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Home'),
+            ),
+          ],
+        ),
     );
   }
 }
